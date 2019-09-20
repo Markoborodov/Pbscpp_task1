dd3d9afda16f5b836bd651cd85b47e2581f449b3
Andrey Markoborodov <aamark99@gmail.com>
2019-09-20 21:42:33 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 class ComplexNumber {
     int Re;
     int Im;
+public:
+    explicit constexpr ComplexNumber(int x = 0, int y = 0) :Re(x), Im(y) { }
+    constexpr void SetRe(int x)
+    {
+        Re = x;
+    }
+    constexpr void SetIm(int x)
+    {
+        Im = x;
+    }
+    constexpr int GetRe() const
+    {
+        return Re;
+    }
+    constexpr int GetIm() const
+    {
+        return Im;
+    }
+    constexpr bool operator==(const ComplexNumber &obj) const {
+        return Re == obj.Re && Im == obj.Im;
+    }
 };
 
 constexpr ComplexNumber Conjugate(const ComplexNumber &x) {
@@ -13,5 +34,6 @@ constexpr ComplexNumber Conjugate(const ComplexNumber &x) {
 int main() {
     constexpr ComplexNumber a(1, 2);
     constexpr ComplexNumber b(1, -2);
-    constexpr auto c = Conjugate(a);static_assert(b == c, "failed");
+    constexpr auto c = Conjugate(a);
+    static_assert(b == c, "failed");
 }
